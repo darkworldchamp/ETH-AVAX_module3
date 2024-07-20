3521a5e3a958469a9ca160b1dd80186fe3dcc60e
Nikhil sharma <140893545+darkworldchamp@users.noreply.github.com>
2024-07-20 16:48:55 +0530

    Update ERC20.sol

--- ERC20.sol
+++ ERC20.sol
@@ -4,24 +4,24 @@ pragma solidity ^0.8.20;
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/ERC20.sol";
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/access/Ownable.sol";
 
-contract MyToken is ERC20("Nikhil's Token","NT"), Ownable {
-    constructor() Ownable(msg.sender){}
+contract MyToken is ERC20("Nikhil's Token", "NT"), Ownable {
+    constructor() Ownable(msg.sender) {}
 
     // Only the owner can mint new tokens
     function mint(address to, uint256 amount) public onlyOwner {
         _mint(to, amount);
     }
-
     // Any user can burn their tokens
     function burn(uint256 amount) public {
         _burn(msg.sender, amount);
     }
-
-    // Any user can burn tokens from a specific address, provided they have allowance
-    function burnFrom(address from, uint256 amount) public {
-        uint256 currentAllowance = allowance(from, msg.sender);
-        require(currentAllowance >= amount, "ERC20: burn amount exceeds allowance");
-        _approve(from, msg.sender, currentAllowance - amount);
-        _burn(from, amount);
+    // Transfer tokens from sender to recipient
+    function transferTokens(address recipient, uint256 amount) public returns (bool) {
+        return transfer(recipient, amount);
+    }
+    // Only the owner can transfer tokens from one address to another with allowance
+    function transferTokensFrom(address sender, address recipient, uint256 amount) public onlyOwner returns (bool) {
+        return transferFrom(sender, recipient, amount);
     }
 }
+
